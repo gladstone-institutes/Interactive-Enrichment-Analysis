e22b8ec384831d0fa1ca7b3aa4cf046598df63cb
Alexander Pico <apico@gladstone.ucsf.edu>
2022-11-29 16:43:45 -0800

    Update launch_app.R

--- launch_app.R
+++ launch_app.R
@@ -1,5 +1,15 @@
 # Verify initial dependencies
-install.packages(c("shinydashboard", "shiny", "shinyjs", "shinyBS", "DT","BiocManager"))
-
+initial.libs <- c("shinydashboard", "shiny", "shinyjs", "shinyBS", "DT","BiocManager")
+options(install.packages.check.source = "no")
+options(install.packages.compile.from.source = "never")
+if (!require("pacman")) install.packages("pacman"); library(pacman)
+  p_load(initial.libs, update = TRUE, try.bioconductor=TRUE, character.only = TRUE)
+  status <- sapply(initial.libs,require,character.only = TRUE)
+  if(all(status))
+     print("All initial libraries successfully installed and loaded.")
+  } else{   
+     print(paste("ERROR: One or more libraries failed to install correctly.",
+           status))
+  }
 # Launch shiny app
 shiny::runGitHub('Interactive-Enrichment-Analysis', 'gladstone-institutes',subdir = 'shiny_run/app')
