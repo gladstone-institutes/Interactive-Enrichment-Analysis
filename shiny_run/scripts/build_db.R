5c54e565030b2a5088f6505c166a8dd22cfbe918
Alexander Pico <apico@gladstone.ucsf.edu>
2022-12-06 17:02:54 -0800

    new db and build scripts

---
+++ shiny_run/scripts/build_db.R
@@ -0,0 +1,38 @@
+# Process provided GMT files and build an RData collection
+
+library(rWikiPathways)
+library(dplyr)
+library(magrittr)
+
+build_db <- function(gmt.list, db.name){
+  
+  #read gmt files and format gmt df objects
+  lapply(gmt.list, function(g){
+    g.name <- gsub(".gmt","", g)
+    g.fn <- file.path("shiny_run","databases","gmts",g)
+    g.tg <- rWikiPathways::readGMT(g.fn)
+    # special handling
+    if(startsWith(g.tg$term[1], "GOBP_")){ #GO GMT
+      g.df <- g.tg %>%
+        dplyr::mutate(term = gsub("GOBP_","",term)) %>%
+        dplyr::mutate(term = gsub("_"," ",term)) %>%
+        dplyr::mutate(term = tolower(term)) %>%
+        dplyr::mutate(name = term)
+    } else if (grepl("%WikiPathways_", g.tg$term[1])) { #WP GMT
+      g.df <- rWikiPathways::readPathwayGMT(g.fn) %>%
+        dplyr::rename(term = wpid) %>%
+        dplyr::select(c(name, term, gene))
+
+    } else if (grepl("^PMC\\d+__.*$", g.tg$term[1])) { # PFOCR
+      g.df <- rWikiPathways::readGMTnames(g.fn) %>%
+        dplyr::right_join(g.tg, by=c("term")) %>%
+        dplyr::mutate(term = gsub(".jpg", "", term)) #if it's there
+    }
+    
+    assign(g.name, g.df)
+  })
+  
+  db.name.list <- gsub(".gmt","", gmt.list)
+  db.fn <- file.path("shiny_run","databases",paste(db.name,"RData", sep = "."))
+  save(list=db.name.list, file=db.fn)
+}
---
+++ shiny_run/scripts/fetch_gmts.sh
@@ -0,0 +1,21 @@
+# Fetches GMT files for building database collections (GH Actions)
+
+## Datestamp for downloads
+DATE=$(date -u +%Y%m%d) #change argto --utc (linux), -u (macOS)
+
+## WikiPathways
+WP_REL=$(curl --silent https://wikipathways-data.wmcloud.org/current/gmt/ | grep -o -m 1 'wikipathways-\d*' | sed 's/wikipathways-//' | head -1)  
+wget -O "shiny_run/databases/gmts/wp_hs_"${DATE}".gmt" "https://wikipathways-data.wmcloud.org/current/gmt/wikipathways-"${WP_REL}"-gmt-Homo_sapiens.gmt"
+wget -O "shiny_run/databases/gmts/wp_mm_"${DATE}".gmt" "https://wikipathways-data.wmcloud.org/current/gmt/wikipathways-"${WP_REL}"-gmt-Mus_musculus.gmt"
+
+##PFOCR
+PFOCR_REL=$(curl --silent https://wikipathways-data.wmcloud.org/pfocr/ | grep -o 'href=".*">pfocr' | sed 's/href=\"//;s/\">pfocr//' | tail -n 1)
+wget -O "shiny_run/databases/gmts/pfocr_hs_"${DATE}".gmt" "https://wikipathways-data.wmcloud.org/pfocr/"${PFOCR_REL}
+
+#Gene Ontology
+GO_HS_REL=$(curl --silent https://www.gsea-msigdb.org/gsea/msigdb/human/collections.jsp | grep -o 'c5\.go\.bp\.v.*\.entrez\.gmt' | sed 's/c5\.go\.bp\.v//;s/\.entrez\.gmt//')
+GO_MM_REL=$(curl --silent https://www.gsea-msigdb.org/gsea/msigdb/mouse/collections.jsp | grep -o 'm5\.go\.bp\.v.*\.entrez\.gmt' | sed 's/m5\.go\.bp\.v//;s/\.entrez\.gmt//')
+wget -O "shiny_run/databases/gmts/go_hs_"${DATE}".gmt" "https://data.broadinstitute.org/gsea-msigdb/msigdb/release/"${GO_HS_REL}"/c5.go.bp.v"${GO_HS_REL}".entrez.gmt"
+wget -O "shiny_run/databases/gmts/go_mm_"${DATE}".gmt" "https://data.broadinstitute.org/gsea-msigdb/msigdb/release/"${GO_MM_REL}"/m5.go.bp.v"${GO_MM_REL}".entrez.gmt"
+
+
