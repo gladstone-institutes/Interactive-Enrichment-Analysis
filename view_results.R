e2b2c07c18ed14316600a20d8a902101d3a8b69c
Alexander Pico <alex.pico@gladstone.ucsf.edu>
2024-07-17 16:37:05 -0700

    Added lib check for results-first scenario

--- view_results.R
+++ view_results.R
@@ -1,4 +1,5 @@
 # Check R version
+message("Checking R version")
 x = readLines("http://cran.r-project.org/sources.html")
 # the version number is in the next line of 'The latest release'
 relver = gsub("(.*R-|\\.\\d\\.tar\\.gz.*)", "", x[grep("latest release", x) + 1L])
@@ -12,6 +13,7 @@ if(vcheck == 1){
   stop(sprintf("Please install R version %s or higher",relver))
 }
 # Check initial dependencies
+message("Checking dependencies")
 initial.libs <- c("shinydashboard", "shiny", "shinyjs", "shinyBS", "DT","BiocManager")
 options(install.packages.check.source = "no")
 options(install.packages.compile.from.source = "never")
@@ -24,5 +26,46 @@ status <- sapply(initial.libs,require,character.only = TRUE)
      print(paste("ERROR: One or more libraries failed to install correctly.",
            status))
   }
+# Additional dependencies loaded during analysis
+p.load.libs <- c(
+  "fs",
+  "writexl",
+  "stringr",
+  "plyr",
+  "tidyr",
+  "dplyr",
+  "magrittr",
+  "rstudioapi",
+  "ggplot2",
+  "ggnewscale")
+bioc.load.libs <- c(
+  "AnnotationDbi",
+  "DOSE",
+  "enrichplot",
+  "clusterProfiler",
+  "rWikiPathways",
+  "EnhancedVolcano")
+
+# Install from cran 
+p_load(p.load.libs, try.bioconductor=TRUE, character.only = TRUE)
+status <- sapply(p.load.libs,require,character.only = TRUE)
+if(all(status)){
+  print("Additional libraries successfully installed and loaded.")
+} else {   
+  print(paste("ERROR: One or more libraries failed to install correctly.",
+              status))
+}
+# Check which bioc packages are not installed
+installed_packages <- installed.packages()[, "Package"]
+packages_to_install <- bioc.load.libs[!(bioc.load.libs %in% installed_packages)]
+
+# Install missing packages
+if (length(packages_to_install) > 0) {
+  BiocManager::install(packages_to_install, update = FALSE, force = TRUE)
+} else {
+  message("All specified packages are already installed.")
+}
+
+
 # Launch shiny app
 shiny::runApp('shiny_result/app', launch.browser= TRUE)
